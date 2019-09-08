237965f767322133af2d170278b7fae54ef76f88
Xwilarg <xwilarg@yahoo.fr>
2019-09-08 21:31:47 +0900

    Add player spawn

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject editorUI;
+
+    public void StartTest()
+    {
+        GameObject spawn = GameObject.FindGameObjectWithTag("Respawn");
+        Camera.main.gameObject.SetActive(false);
+        editorUI.SetActive(false);
+        GameObject player = Instantiate(playerPrefab, spawn.transform);
+        player.transform.Translate(0f, 1f, 0f);
+    }
+}
