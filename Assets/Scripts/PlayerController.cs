bb464bc07b276004e34419f168b4d57fe6b98b48
Xwilarg <xwilarg@yahoo.fr>
2019-09-08 20:51:56 +0900

    Add character rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@
 public class PlayerController : MonoBehaviour
 {
     private CharacterController cc;
-    private const float speed = 5f;
+    private const float speed = 5f; // Character speed
+    private const float mouseYMultiplicator = -20f; // Character rotation (Y axis, vertical)
+    private const float mouseXMultiplicator = 40f; // Character rotation (X axis, horizontal)
 
     private void Start()
     {
         cc = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void Update()
     {
+        // Debug: Release mouse
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Move character
         Vector3 move = Vector3.zero;
         if (cc.isGrounded)
         {
@@ -20,5 +33,7 @@ public class PlayerController : MonoBehaviour
         }
         move.y -= -Physics.gravity.y * Time.deltaTime;
         cc.Move(move * Time.deltaTime);
+        // Rotate character
+        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * mouseYMultiplicator, Input.GetAxis("Mouse X") * mouseXMultiplicator, 0f) * Time.deltaTime);
     }
 }
