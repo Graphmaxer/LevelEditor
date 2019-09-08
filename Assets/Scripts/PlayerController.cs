50cd53b239f738a9f2bcd10c81bb1fd101823167
Xwilarg <xwilarg@yahoo.fr>
2019-09-08 19:01:06 +0900

    Add player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController cc;
+    private const float speed = 5f;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    public void Update()
+    {
+        Vector3 move = Vector3.zero;
+        if (cc.isGrounded)
+        {
+            move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * speed;
+        }
+        move.y -= -Physics.gravity.y * Time.deltaTime;
+        cc.Move(move * Time.deltaTime);
+    }
+}
